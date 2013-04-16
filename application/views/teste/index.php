5b44556f3cfd2b52649b7116e4cce72f176bdd45
franciscolucas <franciscolucas@lucas.(none)>
2013-04-15 21:51:26 -0300

    auto3

--- application/views/teste/index.php
+++ application/views/teste/index.php
@@ -1,7 +1,58 @@
 <html>
 <head>
 <title>jQuery autocomplete with JSON / JSONP, overriding q and limit parameters to use geoNames.org</title>
+<style>
+.ac_results {
+  padding: 0px;
+  border: 1px solid black;
+  background-color: white;
+  overflow: hidden;
+  z-index: 99999;
+}
 
+.ac_results ul {
+  width: 100%;
+  list-style-position: outside;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.ac_results li {
+  margin: 0px;
+  padding: 2px 5px;
+  cursor: default;
+  display: block;
+  /* 
+  if width will be 100% horizontal scrollbar will apear 
+  when scroll mode will be used
+  */
+  /*width: 100%;*/
+  font: menu;
+  font-size: 12px;
+  /* 
+  it is very important, if line-height not setted or setted 
+  in relative units scroll will be broken in firefox
+  */
+  line-height: 16px;
+  overflow: hidden;
+}
+
+.ac_loading {
+  background: white url('indicator.gif') right center no-repeat;
+}
+
+.ac_odd {
+  background-color: #eee;
+}
+
+.ac_over {
+  background-color: #0A246A;
+  color: white;
+}
+</style>
+<? $this->load->view('inc/head.php') ?>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script type="text/javascript">
   $(document).ready(function() {
   $("#city").autocomplete("http://ws.geonames.org/searchJSON", {
@@ -36,11 +87,6 @@
 </head>
 <body>
 This is a demonstration of a few things:
-<ul>
-  <li>hacking the jQuery autocomplete plugin to support JSONP data</li>
-  <li>Overriding the default q and limit parameters that the autocomplete plugin sends (they're hard-coded!)</li>
-  <li>Toying with the geonames.org web service, which allows you to do all sorts of great searches on geographic data</li>
-</ul>
 <form name="test" action="">
         <input id="city" name="city"> Type the start of a US city name in this field...
 </form>
