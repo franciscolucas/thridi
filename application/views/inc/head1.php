55540470cd20ca87fe82cf14d31eac8e28b8a8ce
franciscolucas <franciscolucas@lucas.(none)>
2013-04-16 00:19:55 -0300

    auto7

--- application/views/inc/head1.php
+++ application/views/inc/head1.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="<?= base_url() ?>assets/site/scripts/lib/jquery-1.5.2.min.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>assets/site/scripts/lib/jquery-ui/js/jquery-ui-1.8.16.custom.min.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>assets/site/scripts/pack.js"></script>
+<script type="text/javascript" src="<?= base_url() ?>assets/site/scripts/jsr_class.js"></script>
