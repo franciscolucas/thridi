7265a207f5846d0c283a6d6d6872bf6361745bc6
tzadi <franlucas.cisco@gmail.com>
2013-10-22 00:21:21 -0200

    commit email
    commit email

--- application/config/email.php
+++ application/config/email.php
@@ -1,6 +1,15 @@
 <?
-$config['protocol'] = 'sendmail';
-$config['mailpath'] = '/usr/sbin/sendmail';
-$config['charset'] = 'iso-8859-1';
-$config['wordwrap'] = TRUE;
+//$config['protocol'] = 'sendmail';
+//$config['mailpath'] = '/usr/sbin/sendmail';
+//$config['charset'] = 'iso-8859-1';
+//$config['wordwrap'] = TRUE;
+$config = Array(
+      'protocol' => 'smtp',
+      'smtp_host' => 'ssl://smtp.googlemail.com',
+      'smtp_port' => 465,
+      'smtp_user' => 'franlucas.cisco@gmail.com',
+      'smtp_pass' => 'oblf1000',
+    );
+
 ?>
+
