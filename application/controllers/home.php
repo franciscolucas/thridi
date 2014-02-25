0ed4b0e9dbe326ccd79e8479d1836799a821249f
tzadi <franlucas.cisco@gmail.com>
2014-02-24 22:54:08 -0300

    comment
    comment

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -13,7 +13,7 @@ class Home extends CI_Controller {
  }
 
     function index() {
-		$data->title = 'Thridi';
+		//$data->title = 'Thridi';
         $this->load->view($this->class.'/index', $data);
     }
 }
