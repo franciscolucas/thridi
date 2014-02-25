4c8bdecc5a3fa3b35539d432f885614d31fe690d
tzadi <franlucas.cisco@gmail.com>
2014-02-24 23:41:49 -0300

    backquewm

--- application/controllers/quemsomos.php
+++ application/controllers/quemsomos.php
@@ -9,14 +9,14 @@ class Quemsomos extends CI_Controller {
         parent::__construct();
 		$this->model = 'blog_model';
 		$this->class = $this->router->class;
-		//$this->title = 'Thridi - Quem Somos';
+		$this->title = 'Thridi - Quem Somos';
 		$this->load->model($this->model);
 
     }
  
     function index() {	
         $data->adminblogs = $this->{$this->model}->get_all_posts();
-        //$data->title = $this->title;
+        $data->title = $this->title;
         dadosGerais($data);
         $this->load->view($this->class.'/index', $data);
     }
