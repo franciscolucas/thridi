09497ce7984bf5444db80bff46aff43ff9dded56
Francisco Lucas <franlucas.cisco@gmail.com>
2014-11-14 11:53:57 -0200

    teste home
    teste home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,17 +4,17 @@
 <title>Thridi</title>
 </head>
 <header>
-<? $this->load->view('inc/head.php') ?>
+<? //$this->load->view('inc/head.php') ?>
 </header>
 
 <body>
 <div id="container">
-    
+    <!--
     <div id="header">
       <div id="logo"><a href="<?= base_url() ?>home"><img src="<?= base_url() ?>assets/site/images/logo.png" width="100" height="50" alt="home"/></a></div>
-      <? $this->load->view('inc/menu.php') ?>
+      <? //$this->load->view('inc/menu.php') ?>
     </div>
-
+    -->
     <div id="linha"></div>
         
     <div id="wrapper" class="die"><img class="fullwidthw" src="<?= base_url() ?>assets/site/images/thridi_006_HOME.jpg" alt="home"/></div>
